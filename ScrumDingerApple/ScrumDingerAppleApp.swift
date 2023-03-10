0eaf994a39d7d9102ab717e5383f08aa264b8311
Jash Dhinoja <jash.dhinoja@gmail.com>
2023-03-10 18:20:10 -0500

    Updated Async calls to SwiftUI 5.5

--- ScrumDingerApple/ScrumDingerAppleApp.swift
+++ ScrumDingerApple/ScrumDingerAppleApp.swift
@@ -15,21 +15,35 @@ struct ScrumDingerAppleApp: App {
         WindowGroup {
             NavigationView{
                 ScrumView(scrums: $store.scrums){
-                    ScrumStore.save(scrums: store.scrums){result in
-                        if case .failure(let error) = result {
-                            fatalError(error.localizedDescription)
+//                    ScrumStore.save(scrums: store.scrums){result in
+//                        if case .failure(let error) = result {
+//                            fatalError(error.localizedDescription)
+//                        }
+//                    }
+                    Task{
+                        do{
+                            try await ScrumStore.save(scrums: store.scrums)
+                        }catch{
+                            fatalError("Error saving Scrums")
                         }
                     }
                 }
             }
-            .onAppear{
-                ScrumStore.load{ result in
-                    switch result {
-                    case .success(let scrums):
-                        store.scrums = scrums
-                    case .failure(let error):
-                        fatalError(error.localizedDescription)
-                    }
+//            .onAppear{
+//                ScrumStore.load{ result in
+//                    switch result {
+//                    case .success(let scrums):
+//                        store.scrums = scrums
+//                    case .failure(let error):
+//                        fatalError(error.localizedDescription)
+//                    }
+//                }
+//            }
+            .task {
+                do{
+                    store.scrums = try await ScrumStore.load()
+                }catch{
+                    fatalError("Error loading scrums")
                 }
             }
         }
